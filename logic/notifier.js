fec1f2e7bfe45271fec38ce677987d900cac101d
STAROSTA Martin <starosta@qex.local>
2017-11-30 23:20:18 +0100

    Finished dummy logic for Kraken notifications.

--- logic/notifier.js
+++ logic/notifier.js
@@ -1,30 +1,44 @@
 const _ = require('underscore');
+const email = require('../notifications/email');
 
 const kraken = require('../exchanges/kraken');
 
-const test = ['OWZ5PN-K6GAA-4CAWD6',
-  'OCLLM7-2TSZH-JYGL4P',
-  'OJBAWF-T6GIB-QTM6ME',
-  'OFLAGM-JTYUM-TV7BJE',
-  'OTQEM6-54YFV-XLN7WL',
-  'O6PEND-AHT76-DQQF4X',
-  'OQG2IX-RDETS-HHDUT2',
-  'AAAAAA-BBBBB-CCCCCC'
-];
+
+/*
+let ordersMemory = [
+  'OGTYC7-N6TIG-RCKKC3',
+  'OENI5Y-VA6NJ-QSWMHE',
+  'OEVS3C-RSF2P-NKZX5D',
+  'O6A4DK-UQAVQ-YVDEQY',
+  'O6A4DK-AAAAA-YVDEQY'
+]; */
+let ordersMemory = [];
 
 const run = () => {
-  getClosedOrderInformation('OQG2IX-RDETS-HHDUT2');
-  /*
-    kraken.getOpenOrders()
-      .then((orders) => {
-        if (orders.result.open) {
-          const closed = _.difference(test, Object.keys(orders.result.open));
-
-          console.log(closed);
+  /*getClosedOrderInformation('OQG2IX-RDETS-HHDUT2');*/
+  kraken.getOpenOrders()
+    .then((orders) => {
+      if (orders.result.open) {
+        const openOrders = Object.keys(orders.result.open);
+        const closedOrders = _.difference(ordersMemory, openOrders);
+        //TODO: Handle difference order if exists
+        if (closedOrders.length > 0) {
+          console.log('There is closed order(s).');
+          console.log(closedOrders);
+          closedOrders.map(closedOrder => {
+            console.log(closedOrder);
+            getClosedOrderInformation(closedOrder);
+          });
+          //getClosedOrderInformation('OGTYC7-N6TIG-RCKKC3');
         }
-      })
-      .catch(error => console.log(error));
-      */
+        //TODO: Load orders to memory
+        ordersMemory = openOrders;
+        console.log('Open orders stored in memory');
+        console.log(ordersMemory);
+        setTimeout(run, 1000 * 60 * 15); //TODO> Set tick in .env
+      }
+    })
+    .catch(error => console.log(error.statusCode));
 };
 
 const getClosedOrderInformation = (txid) => {
@@ -32,9 +46,32 @@ const getClosedOrderInformation = (txid) => {
     .then((information) => {
       if (information) {
         console.log(information);
+        for (let key in information) {
+          if (information.hasOwnProperty(key)) {
+            console.log('Sending information.');
+            const orderInformation = information[key];
+            email.sendEmail({
+              to: process.env.OwnerEmail,
+              subject: 'Notifier: Your order has been closed',
+              html: `<p>Your open order has been closed:</p>
+               <p>
+              Symbol:${orderInformation.descr.pair}<br/>
+              Amount:${orderInformation.vol_exec}<br/>
+              Price:${orderInformation.descr.price}<br/>
+              </p>`,
+            });
+          }
+        };
       }
     })
-    .catch(error => console.log(error));
+    .catch((error) => {
+      if (error.statusCode === 504 || error.statusCode === 520) {
+        console.log(`Error ${error.statusCode} getting order information. Will try again after 5 seconds.`)
+        setTimeout(() => {
+          getClosedOrderInformation(txid);
+        }, 5000);
+      }
+    });
 }
 module.exports = {
   run,
